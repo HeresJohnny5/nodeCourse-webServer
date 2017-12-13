cb6987e3af4e36a7dc8e1824030690ae44de4716
John Erickson <ericksonjohn08@hotmail.com>
2017-12-13 12:42:31 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+			return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+// middlewear
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+	res.render('home', {
+		pageTitle: 'Home Page',
+		welcomeMessage: "Hello there friend!"
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		pageTitle: 'About Page',
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		message: "404 Error"
+	});
+});
+
+app.listen(3000, () => {
+	console.log("Server has started!");
+});
